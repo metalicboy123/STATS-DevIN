9163a8c9c070d592bfd2935087ba226cd9c2d580
kjhermano <kjhermano@users.noreply.github.com>
2018-11-15 11:06:27 +0700

    First Commit

---
+++ app/src/main/java/edu/sti/stats/SplashActivity.java
@@ -0,0 +1,16 @@
+package edu.sti.stats;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+
+    }
+}
